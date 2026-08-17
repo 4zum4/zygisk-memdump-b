c8f233e01433b5a79aeb10eee3b025e80b88a2ab
4zum4 <130235743+4zum4@users.noreply.github.com>
2026-08-17 11:19:26 +0700

    refactor ELF rebuild metadata model

--- module/src/main/cpp/SoFixer/ElfRebuilder.h
+++ module/src/main/cpp/SoFixer/ElfRebuilder.h
@@ -3,67 +3,63 @@
 //                     Created by F8LEFT on 2017/6/4.
 //                   Copyright (c) 2017. All rights reserved.
 //===----------------------------------------------------------------------===//
-// Rebuild elf file with ElfReader
+// Rebuild ELF files dumped from process memory.
 //===----------------------------------------------------------------------===//
 
 #ifndef SOFIXER_ELFREBUILDER_H
 #define SOFIXER_ELFREBUILDER_H
 
 #include <cstdint>
-#include <vector>
 #include <string>
+#include <vector>
+
 #include "ObElfReader.h"
 
-
-
 #define SOINFO_NAME_LEN 128
+
 struct soinfo {
 public:
     const char* name = "name";
     const Elf_Phdr* phdr = nullptr;
     size_t phnum = 0;
     Elf_Addr entry = 0;
-    uint8_t * base = 0;
+    uint8_t* base = nullptr;
     unsigned size = 0;
 
-    Elf_Addr min_load;
-    Elf_Addr max_load;
-
-    uint32_t unused1 = 0;  // DO NOT USE, maintained for compatibility.
+    Elf_Addr min_load = 0;
+    Elf_Addr max_load = 0;
 
     Elf_Dyn* dynamic = nullptr;
     size_t dynamic_count = 0;
     Elf_Word dynamic_flags = 0;
 
-    uint32_t unused2 = 0; // DO NOT USE, maintained for compatibility
-    uint32_t unused3 = 0; // DO NOT USE, maintained for compatibility
-
-    unsigned flags = 0;
-
     const char* strtab = nullptr;
-    Elf_Sym* symtab = nullptr;
-
-    uint8_t * hash = 0;
     size_t strtabsize = 0;
+    Elf_Sym* symtab = nullptr;
+    size_t symtab_count = 0;
+
+    uint8_t* hash = nullptr;
     size_t nbucket = 0;
     size_t nchain = 0;
     unsigned* bucket = nullptr;
     unsigned* chain = nullptr;
 
-    Elf_Addr * plt_got = nullptr;
+    uint8_t* gnu_hash = nullptr;
+
+    Elf_Addr* plt_got = nullptr;
 
     uint32_t plt_type = DT_REL;
-    Elf_Rel* plt_rel = nullptr;
+    void* plt_rel = nullptr;
+    size_t plt_rel_size = 0;
     size_t plt_rel_count = 0;
-    Elf_Rela* plt_rela = nullptr;
-    size_t plt_rela_count = 0;
 
     Elf_Rel* rel = nullptr;
     size_t rel_count = 0;
+    Elf_Rela* rela = nullptr;
+    size_t rela_count = 0;
 
     void* preinit_array = nullptr;
     size_t preinit_array_count = 0;
-
     void** init_array = nullptr;
     size_t init_array_count = 0;
     void** fini_array = nullptr;
@@ -72,30 +68,33 @@ public:
     void* init_func = nullptr;
     void* fini_func = nullptr;
 
-    // ARM EABI section used for stack unwinding.
-    Elf_Addr * ARM_exidx = nullptr;
+    Elf_Addr* ARM_exidx = nullptr;
     size_t ARM_exidx_count = 0;
+
     unsigned mips_symtabno = 0;
     unsigned mips_local_gotno = 0;
     unsigned mips_gotsym = 0;
 
-    // When you read a virtual address from the ELF file, add this
-    // value to get the corresponding address in the process' address space.
-    uint8_t * load_bias = nullptr;
+    uint8_t* load_bias = nullptr;
 
     bool has_text_relocations = false;
     bool has_DT_SYMBOLIC = false;
 };
 
-
 class ElfRebuilder {
 public:
-    ElfRebuilder(ObElfReader* elf_reader);
-    ~ElfRebuilder() { if(rebuild_data != nullptr) delete []rebuild_data; }
+    explicit ElfRebuilder(ObElfReader* elf_reader);
+    ~ElfRebuilder() {
+        if (rebuild_data != nullptr) delete[] rebuild_data;
+    }
+
     bool Rebuild();
 
     void* getRebuildData() { return rebuild_data; }
     size_t getRebuildSize() { return rebuild_size; }
+
+    void setPatchInit(bool b) { isPatchInit = b; }
+
 private:
     bool RebuildPhdr();
     bool RebuildShdr();
@@ -103,40 +102,40 @@ private:
     bool RebuildRelocs();
     bool RebuildFin();
 
-  template <bool isRela>
-  void relocate(uint8_t * base, Elf_Rel* rel, Elf_Addr dump_base);
+    template <bool isRela>
+    void relocate(uint8_t* base, Elf_Rel* rel, Elf_Addr dump_base);
+
+    Elf_Word AddSection(const char* name, Elf_Word type, Elf_Xword flags,
+                        Elf_Addr addr, Elf_Xword size, Elf_Xword align,
+                        Elf_Xword entsize = 0);
+    void SortSectionsAndFixLinks();
+
     ObElfReader* elf_reader_;
     soinfo si;
 
-    int rebuild_size = 0;
-    uint8_t * rebuild_data = nullptr;
+    size_t rebuild_size = 0;
+    uint8_t* rebuild_data = nullptr;
 
     Elf_Word sDYNSYM = 0;
     Elf_Word sDYNSTR = 0;
     Elf_Word sHASH = 0;
+    Elf_Word sGNUHASH = 0;
     Elf_Word sRELDYN = 0;
     Elf_Word sRELADYN = 0;
     Elf_Word sRELPLT = 0;
-    Elf_Word sPLT = 0;
     Elf_Word sTEXTTAB = 0;
     Elf_Word sARMEXIDX = 0;
     Elf_Word sFINIARRAY = 0;
     Elf_Word sINITARRAY = 0;
     Elf_Word sDYNAMIC = 0;
-    Elf_Word sGOT = 0;
     Elf_Word sDATA = 0;
-    Elf_Word sBSS = 0;
     Elf_Word sSHSTRTAB = 0;
 
     std::vector<Elf_Shdr> shdrs;
     std::string shstrtab;
 
-  unsigned external_pointer = 0;
-private:
+    unsigned external_pointer = 0;
     bool isPatchInit = false;
-public:
-    void setPatchInit(bool b) { isPatchInit = b; }
 };
 
-
-#endif //SOFIXER_ELFREBUILDER_H
+#endif // SOFIXER_ELFREBUILDER_H
